54f995655b7ab4a14e99753e42eb006ff6e1922c
Daniel Zilberman <dzilberm@amazon.com>
2023-02-03 10:56:02 -0800

    DZ: initial commit of verified EKS Terraform code into new repository

---
+++ examples/eks-cluster-with-new-vpc/main.tf
@@ -0,0 +1,172 @@
+provider "aws" {
+  region = local.region
+}
+
+provider "kubernetes" {
+  host                   = module.eks_blueprints.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks_blueprints.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks_blueprints.eks_cluster_id
+}
+
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+locals {
+  name = basename(path.cwd)
+  # var.cluster_name is for Terratest
+  # DZ: add unique postfix to avioid duplication or 'global' role names derived from a cluster name
+  # cluster_name = coalesce(var.cluster_name, local.name)
+  
+  cluster_name1 = coalesce(var.cluster_name, local.name)
+  #DZ: add unique suffix at the end of cluster_name1
+  cluster_name = "${local.cluster_name1}-dz2"
+  
+  #DZ: us-west-1 region had consistent compute node deployment failures  
+  region       = "us-west-1"
+
+  vpc_cidr = "10.0.0.0/16"
+  #azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+  #DZ reduce number of AZs to poll for other than us-west-2 regions
+  azs      = slice(data.aws_availability_zones.available.names, 0, 2)
+
+  tags = {
+    Blueprint  = local.name
+    GithubRepo = "github.com/dzilbermanvmw/terraform-aws-eks-blueprints"
+  }
+}
+
+#---------------------------------------------------------------
+# EKS Blueprints
+#---------------------------------------------------------------
+
+module "eks_blueprints" {
+  source = "../.."
+
+   
+  cluster_name    = local.cluster_name
+  # DZ: current version of K8s is 1.24 - may want to change it here
+  cluster_version = "1.24"
+
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnets
+
+  # DZ: configuration for data plane: name, AMI type and limits   
+  managed_node_groups = {
+    mg_5 = {
+      node_group_name = "managed-ondemand"
+      #DZ: instance AMI type, min, amx and desire number of compute nodes and ASG parameters - up the node size for fluentbit etc.
+      instance_types  = ["m5.2xlarge"]
+      min_size        = 2
+      max_size        = 4
+      desired_size    = 3
+      subnet_ids      = module.vpc.private_subnets
+    }
+  }
+
+  tags = local.tags
+} #DZ end of eks_blueprints module
+
+#DZ: EKS add-ons module below
+    
+module "eks_blueprints_kubernetes_addons" {
+  source = "../../modules/kubernetes-addons"
+
+  eks_cluster_id       = module.eks_blueprints.eks_cluster_id
+  eks_cluster_endpoint = module.eks_blueprints.eks_cluster_endpoint
+  eks_oidc_provider    = module.eks_blueprints.oidc_provider
+  eks_cluster_version  = module.eks_blueprints.eks_cluster_version
+
+  # EKS Managed Add-ons
+  enable_amazon_eks_vpc_cni            = true
+  enable_amazon_eks_coredns            = true
+  enable_amazon_eks_kube_proxy         = true
+  enable_amazon_eks_aws_ebs_csi_driver = true
+
+  # Add-ons
+  enable_aws_load_balancer_controller = true
+  enable_metrics_server               = true
+  enable_aws_cloudwatch_metrics       = true
+  enable_kubecost                     = true
+  enable_gatekeeper                   = true
+
+  enable_cluster_autoscaler = true
+  cluster_autoscaler_helm_config = {
+    set = [
+      {
+        name  = "podLabels.prometheus\\.io/scrape",
+        value = "true",
+        type  = "string",
+      }
+    ]
+  }
+
+  enable_cert_manager = true
+  cert_manager_helm_config = {
+    set_values = [
+      {
+        name  = "extraArgs[0]"
+        value = "--enable-certificate-owner-ref=false"
+      },
+    ]
+  }
+  # TODO - requires dependency on `cert-manager` for namespace
+  # enable_cert_manager_csi_driver = true
+
+  tags = local.tags
+} # DZ end of eks_blueprints_kubernetes_addons module
+
+#---------------------------------------------------------------
+# Supporting Resources
+#---------------------------------------------------------------
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 10)]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  # Manage so we can name
+  manage_default_network_acl    = true
+  default_network_acl_tags      = { Name = "${local.name}-default" }
+  manage_default_route_table    = true
+  default_route_table_tags      = { Name = "${local.name}-default" }
+  manage_default_security_group = true
+  default_security_group_tags   = { Name = "${local.name}-default" }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
+
+  tags = local.tags
+}
