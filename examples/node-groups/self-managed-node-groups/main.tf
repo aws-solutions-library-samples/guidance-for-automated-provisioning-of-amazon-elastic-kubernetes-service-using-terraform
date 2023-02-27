5ab86db47ae8ea4a4f7327cd7d65e1967e82ff79
Daniel Zilberman <42846514+dzilbermanvmw@users.noreply.github.com>
2023-02-27 14:18:43 -0800

    Update main.tf
    Added comments, updated link to Githubrepo to this repo

--- examples/node-groups/self-managed-node-groups/main.tf
+++ examples/node-groups/self-managed-node-groups/main.tf
@@ -24,6 +24,7 @@ data "aws_availability_zones" "available" {}
 
 locals {
   name   = basename(path.cwd)
+  #DZ: update the value to a target region if needed
   region = "us-west-2"
 
   vpc_cidr = "10.0.0.0/16"
@@ -31,7 +32,8 @@ locals {
 
   tags = {
     Blueprint  = local.name
-    GithubRepo = "github.com/aws-ia/terraform-aws-eks-blueprints"
+    #DZ: need to point to AWS Solutions Library repo here 
+    GithubRepo = "github.com/aws-solutions-library-samples/guidance-for-automated-provisioning-of-amazon-elastic-kubernetes-service-using-terraform"
   }
 }
 
@@ -42,7 +44,8 @@ module "eks_blueprints" {
   source = "../../.."
 
   cluster_name    = local.name
-  cluster_version = "1.23"
+  #DZ: update to a target EKS K8s API version
+  cluster_version = "1.24"
 
   vpc_id             = module.vpc.vpc_id
   private_subnet_ids = module.vpc.private_subnets
